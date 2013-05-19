5012d3cda0d8b3c6f5fbec5b946d8fa9109e4839
Vachan Kudmule <vachan@kdc.in>
2013-05-19 10:44:35 +0530

    First Commit
    Still need to confrim with LIVE testing if CSS required

---
+++ wp-webrupee.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WP WebRupee
+Plugin URI: http://www.kdclabs.com/?p=74
+Description: WP WebRupee provides a simple, cross browser method for using the Rupee symbol on your WordPress. It converts all the Rs / Rs. symbol to Rupee Symbol.
+Version: 1.0.0
+Author: _KDC-Labs
+Author URI: http://www.kdclabs.com/
+License: GNU General Public License v2.0
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+add_action( 'wp_enqueue_scripts', 'wp_webrupee' );
+
+function wp_webrupee() {
+	wp_enqueue_style( 'wp-webrupee',  plugins_url('assets/js/webrupee.js', __FILE__) );
+}
